74cb4a0c5bf703a35aeda09389ddca40f42eb252
Leeymattos <weslley_mattosibf@hotmail.com>
2022-09-22 23:57:04 -0300

    first commit

---
+++ src/atividade1.ts
@@ -0,0 +1,21 @@
+import PromptSync from 'prompt-sync'
+
+const prompt = PromptSync()
+
+console.log('Armazenar dados da Array')
+
+console.log('Remover dados da Array')
+
+console.log('Atualizar dados da Array')
+
+console.log('Apresentar todos os dados da Array')
+
+const vet: string[] = []
+
+
+let option = 0
+
+
+option = Number(prompt('-->'))
+
+
